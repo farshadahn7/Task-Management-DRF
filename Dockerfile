120dafcba23dab3123049bd7693ecf4c14db5083
farshadahn7 <farshadahn7@gmail.com>
2025-01-21 15:55:14 +0330

    Postgres configured and accounts app created and model of user has been written and migrated

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,12 @@
 FROM python:3.11.4-slim-buster
 
-RUN apt-get update \
-    && apt-get -y install libpq-dev gcc
-
 ENV PYTHONDONTWRITEBYTECODE='1'
 ENV PYTHONUNBUFFERED='1'
 
 WORKDIR /app/
 RUN pip install --upgrade pip
 COPY ./.env /app/
+
 COPY ./requirements.txt /app/
 RUN pip install -r requirements.txt
 COPY ./core /app/
--- core/core/settings.py
+++ core/core/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Installed app
+    'rest_framework',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Custom user model
+AUTH_USER_MODEL = 'accounts.CustomUser'
