5843021e3ff071c13b8366510d1923ca1123c69f
farshadahn7 <farshadahn7@gmail.com>
2025-01-21 21:16:46 +0330

    drf-yasg installed and configured

--- core/core/settings.py
+++ core/core/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'mail_templated',
+    'rest_framework_swagger',
+    'drf_yasg',
 
     # Local apps
     'accounts.apps.AccountsConfig',
