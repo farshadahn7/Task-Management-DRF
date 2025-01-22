8377b334ff6f8eea587e8c87b1bc6d1c28c5a46a
farshadahn7 <farshadahn7@gmail.com>
2025-01-22 17:09:17 +0330

    search filter added.

--- core/tasks/api/v1/views.py
+++ core/tasks/api/v1/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
-
+from rest_framework import filters
 
 from ...models import Task
 from .serializers import TaskSerializers
@@ -11,5 +11,6 @@ class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializers
     queryset = Task.objects.all()
     lookup_field = 'slug'
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['status', 'due_date']
+    search_fields = ['title']
